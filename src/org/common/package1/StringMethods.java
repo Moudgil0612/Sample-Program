c91ce0fa993e1ed7216c09537dae6b8522702f77
Ayush.S <Ayush.S@360NDCPC155.360logica.com>
2019-07-10 10:00:46 +0530

    Code is ready to commit

---
+++ src/org/common/package1/StringMethods.java
@@ -0,0 +1,15 @@
+package org.common.package1;
+
+public class StringMethods {
+	
+	public static void main(String[] args) {
+		
+		String str="Apple, Banana, Kiwi";
+		
+		System.out.println(str.substring(15));
+		
+		
+		
+	}
+
+}
