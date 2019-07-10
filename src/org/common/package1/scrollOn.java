a6143250e0ae5f689507442d000c98cdac7725b1
Ayush.S <Ayush.S@360NDCPC155.360logica.com>
2019-07-10 09:09:57 +0530

    Hi pushing my code

---
+++ src/org/common/package1/scrollOn.java
@@ -0,0 +1,39 @@
+package org.common.package1;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class scrollOn {
+	
+	  public static void main(String[] args) throws InterruptedException {  
+                
+	        System.setProperty("webdriver.gecko.driver",".\\exefiles\\geckodriver.exe" );  
+	              
+	        // Initialize Gecko Driver using Desired Capabilities Class  
+	            DesiredCapabilities capabilities = DesiredCapabilities.firefox();  
+	            capabilities.setCapability("marionette",true);  
+	            WebDriver driver= new FirefoxDriver();
+	          
+	        // Launch Website  
+	        driver.navigate().to("https://www.samsung.com/in/");   
+	        
+	  
+	        //Scroll down the webpage by 4500 pixels  
+	        JavascriptExecutor js = (JavascriptExecutor)driver;  
+	        
+	        for(int i=1;i<5;i++)
+	        {
+	        Thread.sleep(3000);
+	        js.executeScript("scrollBy(0, 500)");  
+	        }  
+	        
+	        for(int i=1;i<6;i++)
+	        {
+	        	Thread.sleep(3000);
+	        	js.executeScript("scrollBy(0, -400)");  
+	        }
+
+}
+}
